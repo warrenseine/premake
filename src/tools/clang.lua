401acb847ca6aa3a70bcc6c59c46002c053585df
Warren Seine <warren.seine@aerys.in>
2013-12-24 20:00:50 +0100

    Add the ability to set custom flags to Clang.

--- src/tools/clang.lua
+++ src/tools/clang.lua
@@ -7,7 +7,8 @@
 	premake.tools.clang = {}
 	local clang = premake.tools.clang
 	local gcc = premake.tools.gcc
-
+	local project = premake.project
+	local config = premake.config
 
 
 --
@@ -20,12 +21,17 @@
 --    An array of C preprocessor flags.
 --
 
+	clang.cppflags = {
+		system = {
+			haiku = "-MMD",
+			wii = { "-MMD", "-MP", "-I$(LIBOGC_INC)", "$(MACHDEP)" },
+			_ = { "-MMD", "-MP" }
+		}
+	}
+
 	function clang.getcppflags(cfg)
-
-		-- Just pass through to GCC for now
-		local flags = gcc.getcppflags(cfg)
+		local flags = config.mapFlags(cfg, clang.cppflags)
 		return flags
-
 	end
 
 
@@ -40,13 +46,48 @@
 --    An array of C compiler flags.
 --
 
+	clang.cflags = {
+		architecture = {
+			x32 = "-m32",
+			x64 = "-m64",
+		},
+		flags = {
+			FatalWarnings = "-Werror",
+			NoFramePointer = "-fomit-frame-pointer",
+			Symbols = "-g"
+		},
+		floatingpoint = {
+			Fast = "-ffast-math",
+			Strict = "-ffloat-store",
+		},
+		kind = {
+			SharedLib = function(cfg)
+				if cfg.system ~= premake.WINDOWS then return "-fPIC" end
+			end,
+		},
+		optimize = {
+			Off = "-O0",
+			On = "-O2",
+			Debug = "-Og",
+			Full = "-O3",
+			Size = "-Os",
+			Speed = "-O3",
+		},
+		vectorextensions = {
+			SSE = "-msse",
+			SSE2 = "-msse2",
+		},
+		warnings = {
+			Extra = "-Wall -Wextra",
+			Off = "-w",
+		}
+	}
+
 	function clang.getcflags(cfg)
-
-		-- Just pass through to GCC for now
-		local flags = gcc.getcflags(cfg)
+		local flags = config.mapFlags(cfg, clang.cflags)
 		return flags
-
 	end
+
 
 
 --
@@ -60,13 +101,19 @@
 --    An array of C++ compiler flags.
 --
 
+	clang.cxxflags = {
+		flags = {
+			NoExceptions = "-fno-exceptions",
+			NoRTTI = "-fno-rtti",
+			NoBufferSecurityCheck = "-fno-stack-protector"
+		}
+	}
+
 	function clang.getcxxflags(cfg)
-
-		-- Just pass through to GCC for now
-		local flags = gcc.getcxxflags(cfg)
+		local flags = config.mapFlags(cfg, clang.cxxflags)
 		return flags
-
 	end
+
 
 
 --
@@ -141,12 +188,38 @@
 --    An array of linker flags.
 --
 
+	clang.ldflags = {
+		architecture = {
+			x32 = { "-m32", "-L/usr/lib32" },
+			x64 = { "-m64", "-L/usr/lib64" },
+		},
+		kind = {
+			SharedLib = function(cfg)
+				local r = { iif(cfg.system == premake.MACOSX, "-dynamiclib", "-shared") }
+				if cfg.system == "windows" and not cfg.flags.NoImportLib then
+					table.insert(r, '-Wl,--out-implib="' .. cfg.linktarget.relpath .. '"')
+				end
+				return r
+			end,
+			WindowedApp = function(cfg)
+				if cfg.system == premake.WINDOWS then return "-mwindows" end
+			end,
+		},
+		system = {
+			wii = { "-L$(LIBOGC_LIB)", "$(MACHDEP)" }
+		}
+	}
+
 	function clang.getldflags(cfg)
+		local flags = config.mapFlags(cfg, clang.ldflags)
 
-		-- Just pass through to GCC for now
-		local flags = gcc.getldflags(cfg)
+		-- Scan the list of linked libraries. If any are referenced with
+		-- paths, add those to the list of library search paths
+		for _, dir in ipairs(config.getlinks(cfg, "system", "directory")) do
+			table.insert(flags, '-L' .. project.getrelative(cfg.project, dir))
+		end
+
 		return flags
-
 	end
 
 
@@ -182,12 +255,14 @@
 --    A list of additional makefile rules.
 --
 
+	clang.makesettings = {
+		system = {
+		}
+	}
+
 	function clang.getmakesettings(cfg)
-
-		-- Just pass through to GCC for now
-		local flags = gcc.getmakesettings(cfg)
-		return flags
-
+		local settings = config.mapFlags(cfg, clang.makesettings)
+		return table.concat(settings)
 	end
 
 
